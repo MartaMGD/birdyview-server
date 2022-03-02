b073a4fd5e556f4422211a8fe1e362efda5cbacc
Marta Gonzalez <martagd96@gmail.com>
2022-03-02 14:55:22 +0100

    crud con delete

--- server/models/birdwatchingmodel.js
+++ server/models/birdwatchingmodel.js
@@ -27,14 +27,6 @@ const birdwatchingSchema = new mongoose.Schema(
             type: String,
             required: true
         },
-        completed: {
-            type: Boolean,
-            default: false,
-        },
-        active: {
-            type: Boolean,
-            default: true
-        },
     }
 )
 
--- server/routes/birdwatching.js
+++ server/routes/birdwatching.js
@@ -6,43 +6,29 @@ const Birdwatching = require("../models/birdwatchingmodel");
 
 // GET METHOD TO RECEIVE BIRDWATCHING INFO
 router.get("/", (req, res) => {
-    const from = req.query.from || 0;
-    const limit = req.query.limit || 3;
-
-    Birdwatching.find({ active: true }, "userId birdname location date hour _id")
-        .skip(Number(from))
-        .limit(Number(limit))
-        .exec((error, birdwatchingDB) => {
-            if (error) {
-                res.status(400).json({
-                    ok: false,
-                    error
-                })
-            } else {
-                res.status(200).json({
-                    ok: true,
-                    birdwatching: birdwatchingDB
-                });
-            }
-        });
+    Birdwatching.find({}, (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result)
+        }
+    });
 });
 
 // POST METHOD TO REGISTER WATCHED BIRD
 router.post("/", async (req, res) => {
     const { userId, birdname, location, date, hour } = req.body;
-    try {
-        const birdwatching = await Birdwatching.create({
-            userId: req.body.userId,
-            birdname: req.body.birdname,
-            location: req.body.location,
-            date: req.body.date,
-            hour: req.body.hour
-        })
-        res.json({ status: 'ok' })
-    } catch (err) {
-        res.json({ status: 'error', error: 'Error' })
-    }
 
+    const birdwatching = new Birdwatching({
+        userId: userId,
+        birdname: birdname,
+        location: location,
+        date: date,
+        hour: hour
+    });
+
+    await birdwatching.save();
+    res.send("Avistamiento añadido")
 });
 
 // PUT METHOD TO UPDATE WATCHED BIRD
